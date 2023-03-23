1daed00e60bc313721e08b308a9be9ca4a9381d8
codingwithRaja <rjali40@gmail.com>
2023-03-23 22:26:39 +0500

    create schema routes and notes api

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,11 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+
 app.get('/', (req, res) => {
     res.send('Hello World From index.js')
 })
@@ -11,3 +16,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on http://localhost:${port}`)
 })
+
