d90e481270d5b653e566f3d5dadc6ea424f9b209
codingwithRaja <rjali40@gmail.com>
2023-03-23 17:40:16 +0500

    nodemon and express server is ready

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+
+
+const mongoose = require('mongoose')
+// require('dotenv').config()
+const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.0"
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('Connected suxxesfully to MongoDB'))
+        .catch(error => console.error('Error connecting to MongoDB', error));
+}
+
+module.exports = connectToMongo;
