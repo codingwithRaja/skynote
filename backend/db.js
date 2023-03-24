bf10c59648b38b615d8bb0fcb9d36815dea7cc0d
codingwithRaja <rjali40@gmail.com>
2023-03-24 15:33:19 +0500

    Express Validator done

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose')
 // require('dotenv').config()
-const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.0"
+const mongoURI = "mongodb://127.0.0.1:27017/skydb?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.0"
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router = express.Router()
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 
-// Create a user using :Post ('/api/auth/) !Does'nt requir auth
-router.post('/', (req, res) => {
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
-    res.send(req.body)
+// Create a user using :Post ('/api/auth/) !Does'nt requir auth/create user /no login required
+router.post('/', [
+    body('email', 'Enter a valid Email').isEmail(),
+    body('name', 'Name must be 3 words').isLength({ min: 3 }),
+    body('password', 'Password Must be atleast 5 digit').isLength({ min: 5 }),
+], (req, res) => {
+    // If there are error,return Bad request and error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    // check whether thr user with this email exists already
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email
+    }).then(user => res.json(user))
+        .catch(err => {
+            console.log(err)
+            res.json({ error: 'This Email already been used', message: err.message })
+        })
+
+
 
 })
 
