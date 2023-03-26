393ff84e987ee426dd167a21cb3468ddfa6891e1
codingwithRaja <rjali40@gmail.com>
2023-03-26 15:01:34 +0500

    initial Nav done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
+// import { Route, Routes } from "react-router-dom";
+// import Home from './components/Home';
+// import About from './components/About';
+
 
 function App() {
   return (
-    <h1>this is my Sky note app</h1>
+    <>
+      <div className='App'>
+        <Navbar />
+        {/* <Routes>
+          <Route exact path="/Home"><Home /></Route>
+          <Route exact path="/About"><About /></Route>
+        </Routes> */}
+        <h1>this is Skynote_app</h1>
+      </div>
+    </>
+
   );
 }
 
