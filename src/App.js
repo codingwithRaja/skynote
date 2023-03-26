53cfe74cce068df608e7bd5740e2dae3bd6f2a68
codingwithRaja <rjali40@gmail.com>
2023-03-26 15:35:18 +0500

    Navbar and routing done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-// import { Route, Routes } from "react-router-dom";
-// import Home from './components/Home';
-// import About from './components/About';
+import { Route, Routes } from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
 
 
 function App() {
@@ -11,10 +11,10 @@ function App() {
     <>
       <div className='App'>
         <Navbar />
-        {/* <Routes>
-          <Route exact path="/Home"><Home /></Route>
-          <Route exact path="/About"><About /></Route>
-        </Routes> */}
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/About" element={<About />}></Route>
+        </Routes>
         <h1>this is Skynote_app</h1>
       </div>
     </>
--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,9 @@ import React from 'react'
 const About = () => {
     return (
         <div>
+            <h1>About vich ao tusss
 
+            </h1>
         </div>
     )
 }
